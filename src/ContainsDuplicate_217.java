b4b164f329f367a342650edc2c958a4281fdb2c2
mateng123 <mateng689@163.com>
2019-07-13 20:01:22 +0800

    217

---
+++ src/ContainsDuplicate_217.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ContainsDuplicate_217 {
+
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num:nums){
+            if(map.containsKey(num)==false){
+                map.put(num,0);
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
