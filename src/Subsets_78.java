928288d18e59aae163b5cf74b7fedbc1c50cd888
mateng123 <mateng689@163.com>
2019-07-15 10:09:47 +0800

    78

--- src/Subsets_78.java
+++ src/Subsets_78.java
@@ -1,20 +1,22 @@
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Subsets_78 {
 
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        ArrayList<Integer> temp = new ArrayList<>();
+        backTrack(nums,temp,res,0);
 
-    //用最大堆
-//    public int findKthLargest(int[] nums, int k) {
-//
-//        PriorityQueue<Integer> queue = new PriorityQueue<>(k,(n1,n2)->{
-//            return n2-n1;
-//        });
-//        for(int num:nums){
-//            queue.offer(num);
-//        }
-//        for(int i =0;i<k-1;i++){
-//            queue.poll();
-//        }
-//        return queue.poll();
-//    }
+        return res;
+    }
+
+    public void backTrack(int[] nums,ArrayList<Integer> temp,List<List<Integer>> res,int first){
+        res.add(new ArrayList<Integer>(temp));
+
+        for(int i=first;i<nums.length;i++){
+            temp.add(nums[i]);
+            backTrack(nums,temp,res,i+1);
+            temp.remove(temp.size()-1);
+        }
+    }
 }
