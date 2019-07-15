452ed6062383772257e00584d0f21a50c56d3ec0
mateng123 <mateng689@163.com>
2019-07-15 10:56:20 +0800

    230

---
+++ src/KthSmallestElementBST_230.java
@@ -0,0 +1,30 @@
+public class KthSmallestElementBST_230 {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+ }
+    
+    int n = 0;
+    int res = -1;
+    public int kthSmallest(TreeNode root, int k) {
+        help(root,k);
+        return res;
+    }
+
+    public void help(TreeNode root,int k){
+        if(root==null){
+            return;
+        }
+
+        kthSmallest(root.left,k);
+        n++;
+        if(n==k){
+            res = root.val;
+        }
+
+        kthSmallest(root.right,k);
+    }
+}
