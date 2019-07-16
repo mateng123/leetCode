8b6d5d66f1fd940ed58b11e5ccc3dac6b7f81822
mateng123 <mateng689@163.com>
2019-07-16 11:44:37 +0800

    142

---
+++ src/LRUCache_146.java
@@ -0,0 +1,8 @@
+import com.sun.xml.internal.bind.v2.TODO;
+
+import java.util.*;
+
+public class LRUCache_146 {
+
+
+}
---
+++ src/LinkedListCycle_ll_142.java
@@ -0,0 +1,44 @@
+public class LinkedListCycle_ll_142 {
+
+    class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+
+    public ListNode detectCycle(ListNode head) {
+        ListNode pos = null;
+        if(head==null||head.next==null){
+            return pos;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        ListNode temp = null;
+        while(fast!=null&&fast.next!=null){
+            fast =fast.next.next;
+            slow = slow.next;
+            if(slow == fast){
+                temp = slow;
+                break;
+            }
+        }
+
+        if(temp == null){
+            return pos;
+        }
+
+        pos = head;
+
+        while(pos!=temp){
+            pos = pos.next;
+            temp = temp.next;
+        }
+
+        return pos;
+    }
+}
