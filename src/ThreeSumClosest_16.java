80bf0a3ee163f30f96ca46e98c158f61906d4aa8
mateng123 <mateng689@163.com>
2019-07-16 16:30:07 +0800

    16

---
+++ src/ThreeSumClosest_16.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class ThreeSumClosest_16 {
+
+    public int threeSumClosest(int[] nums, int target) {
+
+        int res=0,temp=Integer.MAX_VALUE;
+
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for(int i=0;i<n-2;i++){
+            int first = i+1,end =n-1;
+            while(first<end){
+
+                int sum = nums[i]+nums[first]+nums[end];
+
+                if(temp>Math.abs(sum-target)){
+                    temp = Math.abs(sum-target);
+                    res = sum;
+                }
+
+                if(sum>target){
+                    end--;
+
+                }else if(sum<target){
+                    first++;
+                }else{
+                    return sum;
+                }
+
+            }
+        }
+        return res;
+    }
+}
