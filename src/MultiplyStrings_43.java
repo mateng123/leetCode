f0b39e89c8bb9c5b0b811a24c57672a29e4b04ba
mateng123 <mateng689@163.com>
2019-07-17 16:47:32 +0800

    43

--- src/MultiplyStrings_43.java
+++ src/MultiplyStrings_43.java
@@ -1,41 +1,39 @@
 public class MultiplyStrings_43 {
 
-    public String addStrings(String num1, String num2) {
+    public String multiply(String num1, String num2) {
+
+
+        if(num1.equals("0")||num2.equals("0")){
+            return "0";
+        }
 
         int length1 = num1.length();
         int length2 = num2.length();
-        int min = length1>length2 ? length2 : length1;
 
-        int max = length1>length2 ? length1 : length2;
-        int i=0;
-        int sum=0;
-        int temp=1;
-        while(i<min){
+        int[] res = new int[length1+length2];
 
-            sum = (num1.charAt(i)-'0')+(num2.charAt(i)-'0')*temp;
-            temp *= 10;
-            i++;
-        }
-        String tempString;
-        if(length1>length2){
-            tempString=num1;
-        }else{
-            tempString=num2;
-        }
 
-        while(i<max){
-            sum = (tempString.charAt(i))*temp;
-            temp*=10;
-            i++;
+        for(int i=length1-1;i>=0;i--){
+            for(int j =length2-1;j>=0;j--){
+                int a = num1.charAt(i) - '0';
+                int b = num2.charAt(j) - '0';
+
+                res[i+j] +=a*b;
+                if(res[i+j]>=10&&(i+j)!=0){
+                    res[i+j-1] += res[i+j] / 10;
+                    res[i+j] %= 10;
+                }
+            }
         }
-        StringBuilder res = new StringBuilder(sum);
-        return res.toString();
+        String ret = "";
+        for(int k=0;k<=res.length-2;k++){
+            ret += res[k];
+        }
+        return ret;
     }
 
     public static void main(String[] args) {
-
-        String string1 = "123";
-        String string2 = "456";
-        System.out.println(new MultiplyStrings_43().addStrings(string1,string2));
+        String a = new MultiplyStrings_43().multiply("789","456");
+        System.out.println(a);
     }
 }
