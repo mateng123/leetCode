8c8e70246fd86a5294b425133bd8fa191660a60e
mateng123 <mateng689@163.com>
2019-07-18 19:45:21 +0800

    326

---
+++ src/PowerThree_326.java
@@ -0,0 +1,14 @@
+
+public class PowerThree_326 {
+
+    public boolean isPowerOfThree(int n) {
+        if(n<1){
+            return false;
+        }
+        while(n%3==0){
+            n=n/3;
+        }
+
+        return n==1;
+    }
+}
