aeff45106745ebf4a2f3f3050bd81cf1ef84a433
mateng123 <mateng689@163.com>
2019-07-19 16:13:39 +0800

    171

---
+++ src/ExcelSheetColumnNumber_171.java
@@ -0,0 +1,16 @@
+
+public class ExcelSheetColumnNumber_171 {
+
+    public int titleToNumber(String s) {
+        int res = 0;
+        int n = s.length();
+        if(n==0){
+            return res;
+        }
+
+        for(int i=0;i<n;i++){
+            res = res*26+ (s.charAt(i)-'A'+1);
+        }
+        return res;
+    }
+}
