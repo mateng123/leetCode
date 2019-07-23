72411b933879ba6dfe771e46a209fdd40c4040ff
mateng123 <mateng689@163.com>
2019-07-23 20:30:35 +0800

    修改一个小错误

---
+++ src/BinaryTreeInorderTraversal_.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class BinaryTreeInorderTraversal_ {
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
+
+    //递归
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> list = new LinkedList<>();
+//         inorderTraversal(list,root);
+//         return list;
+//     }
+
+//     public void inorderTraversal(List<Integer> list,TreeNode root){
+//         if(root==null){
+//             return;
+//         }
+//         inorderTraversal(list,root.left);
+//         list.add(root.val);
+//         inorderTraversal(list,root.right);
+//     }
+
+    //迭代
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode cur = root;
+        while(!stack.isEmpty()||cur!=null){
+            while(cur!=null){
+                stack.push(cur);
+                cur = cur.left;
+            }
+            TreeNode p = stack.pop();
+            list.add(p.val);
+            cur = p.right;
+        }
+        return list;
+    }
+}
