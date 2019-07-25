35bb791b119bf9931114d6f45ea451d798866a50
mateng123 <mateng689@163.com>
2019-07-25 16:15:36 +0800

    328

---
+++ src/OddEvenLinkedList_328.java
@@ -0,0 +1,27 @@
+
+public class OddEvenLinkedList_328 {
+
+    public static class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null||head.next==null){
+            return head;
+        }
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode firstEven = even;
+        while(even!=null&&even.next!=null){
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = firstEven;
+        return head;
+    }
+}
