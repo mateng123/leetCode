43ba89e035db87ede56fca99959f6b7697d4ab15
mateng123 <mateng689@163.com>
2019-07-28 20:48:48 +0800

    17

---
+++ src/LetterCombinationsPhoneNumber_17.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class LetterCombinationsPhoneNumber_17 {
+    Map<String, String> phone = new HashMap<String, String>() {{
+        put("2", "abc");
+        put("3", "def");
+        put("4", "ghi");
+        put("5", "jkl");
+        put("6", "mno");
+        put("7", "pqrs");
+        put("8", "tuv");
+        put("9", "wxyz");
+    }};
+
+    List<String> output = new ArrayList<String>();
+
+    public List<String> letterCombinations(String digits) {
+        if(digits.length()!=0)
+            backtrack("",digits);
+        return output;
+    }
+
+    public void backtrack(String str,String digits){
+        if(digits.length()==0){
+            output.add(str);
+            return;
+        }else{
+            String nextDigit = digits.substring(0,1);
+            String letters = phone.get(nextDigit);
+            for(int i=0;i<letters.length();i++){
+                String letter = phone.get(nextDigit).substring(i,i+1);
+                backtrack(str+letter,digits.substring(1));
+            }
+        }
+
+    }
+}
