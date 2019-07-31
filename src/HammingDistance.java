c57e457c9c51a743a808baf694f8cb9a82aa6be3
mateng123 <mateng689@163.com>
2019-07-31 11:12:12 +0800

    461

---
+++ src/HammingDistance.java
@@ -0,0 +1,13 @@
+
+public class HammingDistance {
+    public int hammingDistance(int x, int y) {
+        int temp = x^y;
+        int res = 0;
+        int mid = 1;
+        for(int i=0;i<32;i++){
+        res += (mid&temp);
+        temp = temp>>1;
+        }
+        return res;
+    }
+}
