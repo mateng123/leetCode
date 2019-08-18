12924c5a934a0649b2d7bdbea635b9fe22680e0a
mateng123 <mateng689@163.com>
2019-08-18 08:33:35 +0800

    437

---
+++ src/PathSumIII_137.java
@@ -0,0 +1,35 @@
+
+public class PathSumIII_137 {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+
+        int total=0;
+        public int pathSum(TreeNode root, int sum) {
+            if(root==null){
+                return total;
+            }
+            Sum(root,sum);
+            pathSum(root.left,sum);
+            pathSum(root.right,sum);
+            return total;
+        }
+
+        public void Sum(TreeNode root,int sum){
+            if(root==null){
+                return;
+            }
+            sum = sum-root.val;
+            if(sum==0){
+                total++;
+            }
+            Sum(root.left,sum);
+            Sum(root.right,sum);
+            
+        }
+}
